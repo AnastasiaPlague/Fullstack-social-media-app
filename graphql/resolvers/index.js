87ea7636058952c17c32fbe07bb543467e4c7483
Anastasia <great-swindler@mail.ru>
2020-10-19 20:15:58 +0300

    Add likes and comments functionality

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,18 @@
 const postsResolvers = require("./posts");
 const usersResolvers = require("./users");
+const commentsResolvers = require("./comments");
 
 module.exports = {
+  Post: {
+    likeCount: ({ likes }) => likes.length,
+    commentCount: ({ comments }) => comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
   Mutation: {
     ...usersResolvers.Mutation,
     ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation,
   },
 };
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/checkAuth");
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -29,6 +29,10 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
+      if (body.trim() === "") {
+        throw new UserInputError("Post body must not be empty");
+      }
+
       const newPost = new Post({
         body,
         user: user.id,
@@ -54,6 +58,26 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //Not liked
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
